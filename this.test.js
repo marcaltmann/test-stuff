831605988b03407f2b1497f45bbad095ffd1a93f
Marc Altmann <marcaltmann@posteo.de>
2019-11-04 20:47:24 +0100

    Add tests

--- this.test.js
+++ this.test.js
@@ -3,3 +3,29 @@ test('this refers to empty object in Jest execution context', () => {
   let expected = {};
   expect(actual).toEqual(expected);
 });
+
+test('this in a function refers to the object the function is called on', () => {
+  expect.assertions(1);
+  let obj = {};
+  function myFunc() {
+    let actual = this;
+    let expected = obj;
+    expect(actual).toEqual(expected);
+  }
+
+  obj.myFunc = myFunc;
+  obj.myFunc();
+});
+
+test('this does not refer to object if function is not called on object', () => {
+  expect.assertions(1);
+  let obj = {};
+  function myFunc() {
+    let actual = this;
+    let expected = global;
+    expect(actual).toEqual(expected);
+  }
+
+  obj.myFunc = myFunc;
+  myFunc();
+});
